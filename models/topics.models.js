a489ff4682d07725df68c6adb5e6b2b2c7bbf12c
micatkinson <147393108+micatkinson@users.noreply.github.com>
2024-01-16 15:08:05 +0000

    add completed api/articles/:article_id/comments endpoint

--- models/topics.models.js
+++ models/topics.models.js
@@ -4,15 +4,15 @@ const fs = require("fs/promises")
 function fetchTopics(){
     return db.query('SELECT * FROM topics').then(({rows}) => {
         return rows;
-    })
+    });
 };
 
 function fetchApi(){
     return fs.readFile("./endpoints.json", 'utf-8')
     .then((data) => {
         return JSON.parse(data)
-    })
-}
+    });
+};
 
 function fetchArticleById(article_id, next){
     const query = ` SELECT * FROM articles
@@ -28,7 +28,6 @@ function fetchArticleById(article_id, next){
     });
 };
 
-
 function fetchArticles(){
     return db.query(`SELECT articles.author, title, articles.article_id, articles.topic, articles.created_at, articles.votes, article_img_url,
                     COUNT(comments.article_id) AS comment_count 
@@ -40,10 +39,22 @@ function fetchArticles(){
                     ORDER BY created_at DESC
                     `).then(({rows}) => {
                     return rows;
-});
-}
+    });
+};
+
+function fetchArticleComments(article_id){
+    const query = `SELECT * FROM comments
+                    WHERE article_id = $1
+                    ORDER BY created_at DESC;`
+    return db.query(query, [article_id])
+    .then((result) => {
+        if (result.rows.length === 0){
+            return Promise.reject({status: 404, msg: 'article_id does not exist'})
+        } else {
+            return result.rows;
+        }
+    });
+};
 
 
-
-
-module.exports = { fetchTopics, fetchApi, fetchArticleById, fetchArticles }
+module.exports = { fetchTopics, fetchApi, fetchArticleById, fetchArticles, fetchArticleComments }
