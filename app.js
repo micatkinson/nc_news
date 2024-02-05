8ba23b7450e0069a359166d639eacdb221977975
micatkinson <147393108+micatkinson@users.noreply.github.com>
2024-02-05 10:06:07 +0000

    allow cross origin resource sharing

--- app.js
+++ app.js
@@ -4,14 +4,15 @@ const Port = 3000
 const { handleCustomErrors, handleBadPsqlErrors, handleInvalidPsqlErrors } = require("./errors.index")
 
 const app = express();
+
 app.use(express.json());
 
 const apiRouter = require("./routes/api-router");
 
 app.use('/api', apiRouter)
 
-app.all('*', function(req , res) {
-    res.status(404).send({msg: 'Not Found'})
+app.all('*', function (req, res) {
+    res.status(404).send({ msg: 'Not Found' })
 })
 
 app.use(handleCustomErrors)
